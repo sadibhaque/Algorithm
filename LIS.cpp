0d829d8455e6823320939ca4329a0d63b71d0ff6
sadibhaque <sadibhaquee@gmail.com>
2023-09-13 18:41:24 +0600

    1

---
+++ LIS.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+int main() 
+{
+    ll arr[] = { 1, 9, 5, 4, 3, 2, 1, 0, 8, 9, 10, 10};
+    ll n = 12;
+    ll lis[n];
+
+    for (int i = 0; i < n; i++){
+        lis[i] = 1;
+        for (int j = 0; j < i; j++){
+            if(arr[j] < arr[i]){
+                lis[i] = max(lis[j]+1,lis[i]); 
+            }
+        }
+    }
+    cout<<"Sequence : ";
+    for (int i = 0; i < n; i++) cout<<lis[i]<<" ";
+
+    cout<<endl<<"LIS : ";
+    sort(lis, lis+n);
+    cout<<lis[n-1]<<endl;
+}
