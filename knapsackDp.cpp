fccaa7bc3dc5657f5c18690c8b1a1f7479304c95
Sadib <111662359+sadibhaque@users.noreply.github.com>
2023-10-19 03:20:08 +0600

    1

--- knapsackDp.cpp
+++ knapsackDp.cpp
@@ -3,25 +3,32 @@ using namespace std;
 
 #define ll long long int
 
-int knapsack(ll x, ll n, ll w[], ll p[]){
-    ll mat[n+1][x+1];
+ll dpKnapsack(ll w[], ll p[], ll c, ll n){
+    ll dp[n+1][c+1];
 
-    for (int i = 0; i <= n; i++){
-        for (int j = 0; j <= x; j++){
-            if(i == 0 || j == 0) mat[i][j] = 0;
-            else if(j < w[i-1]) mat[i][j] = mat[i-1][j];
-            else mat[i][j] = max(mat[i-1][j] ,  p[i-1]+ mat[i-1][j-w[i-1]]);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= c; j++)
+        {
+            if (i == 0 || j == 0) dp[i][j] = 0;
+            else if (j < w[i - 1]) dp[i][j] = dp[i - 1][j];
+            else dp[i][j] = max(dp[i - 1][j], p[i - 1] + dp[i - 1][j - w[i - 1]]);
         }
     }
-    return mat[n][x];
+    return dp[n][c];
 }
 
-int main() {
-    ll w[] = {20,10,30},p[] = {32,9,93};
-    ll n = sizeof(w)/sizeof(w[0]);
-    ll cap = 25;
 
-    cout<<knapsack(cap,n,w,p)<<endl;
+int main()
+{
+
+    ll w[] = {3, 3, 2, 5, 1};
+    ll p[] = {10, 15, 10, 12, 8};
+    ll c = 10;
+    ll n = sizeof(p) / sizeof(p[0]);
+
+    cout << greedyKnapsack(w, p, c, n) << endl;
+    cout << dpKnapsack(w, p, c, n) << endl;
 
     return 0;
 }
