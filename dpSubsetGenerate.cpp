464a27496b475a29c9dc17b30aae00b16054509d
Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-02 22:50:14 +0600

    1

---
+++ dpSubsetGenerate.cpp
@@ -0,0 +1,90 @@
+/* Each conquered bug is a step towards mastery. */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+typedef pair<int,int> pi;
+typedef pair<string,int> ps;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<ps> vps;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n";
+#define psum cout<<sum<<"\n";
+#define pans cout<<ans<<"\n";
+#define ptmp cout<<tmp<<"\n";
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define sortv(x) sort((x).begin(),(x).end())
+#define sorta(x,n) sort((x),(x)+(n))
+#define revv(x) reverse((x).begin(),(x).end())
+#define reva(x,n) reverse((x),(x)+(n))
+#define all(x) (x).begin(),(x).end()
+
+vvi ans;
+
+void generate(vi& subset, int i, vi& arr){
+    if(i == arr.size()){
+        ans.pb(subset);
+        return;
+    }
+
+    generate(subset,i+1,arr);
+    subset.pb(arr[i]);
+    generate(subset,i+1,arr);
+    subset.pop_back();
+}
+
+void solve(){
+    int n;
+    cin>>n;
+
+    vi arr(n);
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    vi empty;
+
+    generate(empty,0,arr);
+
+    for(auto it : ans){
+        for(auto ch : it){
+            cout<<ch<<" ";
+        }
+        pend;
+    }
+}
+
+int sadib(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
